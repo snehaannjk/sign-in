f3fbfa4d086c8321aad6b03e0f9401d265812897
snehaannjk <snehaannajacob1968@gmail.com>
2025-04-05 16:06:37 +0530

    Delete sign in directory

--- sign in/script.js
+++
@@ -1,7 +0,0 @@
-function clickSubmitButton(event){
-    event.preventDefault();
-    const username = document.getElementById("username").value;
-    const welcomeMessage= document.getElementById('welcome-message');
-    welcomeMessage.textContent= 'Welcome,' +username + '!';
-    welcomeMessage.style.display = 'block';
-}
